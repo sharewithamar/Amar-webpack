b69ccd2cdc1a52921520a0bd84b214d7bb438bf1
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-06-30 16:04:09 +0530

    code splitting basics

--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,8 +1,21 @@
 //const { sum } = require('./sum');
 
-import sum from './sum';
+/*import sum from './sum';
 import './image_viewer';
 
 let res = sum(5, 10);
 
-console.log(res);
+console.log(res);*/
+
+
+const button = document.createElement('button');
+button.innerText = 'click me';
+button.onclick = () => {
+
+    System.import('./image_viewer.js').then(module => {
+        console.log(module);
+        module.default();
+    });
+};
+
+document.body.appendChild(button);
