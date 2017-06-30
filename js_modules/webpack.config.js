5e228a2dfb999a917a02722329550345a108a245
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-06-30 14:12:41 +0530

    image-webpack-loader,url-loader,file-loader

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -7,7 +7,8 @@ const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/'
     },
     module: {
         rules: [
@@ -22,6 +23,18 @@ const config = {
                     loader: 'css-loader'
                 }),
                 test: /\.css$/
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { limit: 40000 }
+
+                    },
+                    'image-webpack-loader'
+
+                ]
             }
         ]
 
