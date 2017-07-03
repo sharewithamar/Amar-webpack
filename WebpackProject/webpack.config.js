a14edd8587485e91cc2d715c86c99df9ae42e77c
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-07-03 17:01:09 +0530

    code splitting base

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -1,8 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  'faker', 'lodash', 'react', 'react-dom', 'react-input-range', 'react-redux', 'react-router', 'redux', 'redux-form', 'redux-thunk'
+];
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: '.src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
