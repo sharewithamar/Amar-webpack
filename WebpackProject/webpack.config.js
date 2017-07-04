5b387f9988c979309dd14145ab8115fa0f2a6f7e
Amar <sharewithamar@gmail.com>
2017-07-04 22:13:15 +0530

    production

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -35,7 +35,11 @@ module.exports = {
      }),
      new HtmlWebpackPlugin({
        template:'src/index.html'
+     }),
+     new webpack.DefinePlugin({
+       'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
      })
+
   ]
 
 
